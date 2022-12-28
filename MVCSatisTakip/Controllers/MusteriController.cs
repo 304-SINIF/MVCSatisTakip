c4b7f600336e33a44bec42547f8b9dfd128dc028
Smart304 <smartsinif@gmail.com>
2022-12-28 20:03:17 +0300

    Kategori Controller ve Ürün Controller kısımları eklendi

---
+++ MVCSatisTakip/Controllers/MusteriController.cs
@@ -0,0 +1,65 @@
+﻿using MVCSatisTakip.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSatisTakip.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        satistakipEntities db = new satistakipEntities();
+        public ActionResult Index(int sayfa=1)
+        {
+            var musteriler = db.tbl_musteri.ToList().ToPagedList(sayfa, 7);
+            return View(musteriler);
+        }
+
+        public ActionResult Musteriekle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Musteriekle(tbl_musteri p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Musteriekle");
+            }
+            var must = db.tbl_musteri.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var musteri=db.tbl_musteri.Find(id);
+            db.tbl_musteri.Remove(musteri);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult musterigetir(int id)
+        {
+            var must = db.tbl_musteri.Find(id);
+            return View("musterigetir", must);
+        }
+
+        public ActionResult Guncelle(tbl_musteri p1)
+        {
+            var must=db.tbl_musteri.Find(p1.MusteriID);
+            must.TC = p1.TC;
+            must.MusteriAdsoyad = p1.MusteriAdsoyad;
+            must.Meslek = p1.Meslek;
+            must.Sehir = p1.Sehir;
+            must.Telno = p1.Telno;
+            must.Adres = p1.Adres;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVCSatisTakip/Controllers/UrunController.cs
@@ -0,0 +1,61 @@
+﻿using MVCSatisTakip.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSatisTakip.Controllers
+{
+    public class UrunController : Controller
+    {
+        satistakipEntities db = new satistakipEntities();
+        // GET: Urun
+        public ActionResult Index(int sayfa = 1)
+        {
+            var urunler = db.tbl_urun.ToList().ToPagedList(sayfa, 7);
+            return View(urunler);
+        }
+
+        public ActionResult Urunekle()
+        {
+            List<SelectListItem> kategoriler = (from i in db.tbl_kategori.ToList()
+                                                select new SelectListItem
+                                                {
+                            Text = i.KategoriAd,
+                            Value = i.KategoriID.ToString()
+                                                }).ToList();
+
+            ViewBag.ktgr=kategoriler;
+            return View();
+
+        }
+
+        [HttpPost]
+
+        public ActionResult Urunekle(tbl_urun p1)
+        {
+            if (p1.Urunad==null ||p1.UrunFiyat==null || p1.UrunStok==null ||p1.Marka==null) { 
+            List<SelectListItem> kategoriler = (from i in db.tbl_kategori.ToList()
+                                                select new SelectListItem
+                                                {
+                                       Text = i.KategoriAd,
+                                Value = i.KategoriID.ToString()
+                                                }).ToList();
+
+            ViewBag.ktgr = kategoriler;
+            return View();
+            }
+            else
+            {
+                var ktg=db.tbl_kategori.Where(m=>m.KategoriID==p1.tbl_kategori.KategoriID).FirstOrDefault();
+                p1.tbl_kategori=ktg;
+                db.tbl_urun.Add(p1);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+        }
+    }
+}
