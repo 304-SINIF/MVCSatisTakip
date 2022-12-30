f428893a6d9b35539dd6e71ee23fa7bf516ea1b1
Smart304 <smartsinif@gmail.com>
2022-12-30 20:08:00 +0300

    satiscontroller eklendi

--- MVCSatisTakip/Controllers/SatisController.cs
+++ MVCSatisTakip/Controllers/SatisController.cs
@@ -24,5 +24,74 @@ namespace MVCSatisTakip.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult satisekle() /*=> View();*/
+        {
+            List<SelectListItem> musteriler = (from i in db.tbl_musteri.ToList()
+                                                select new SelectListItem
+                                                {
+                                Text = i.MusteriAdsoyad,
+                      Value = i.MusteriID.ToString()
+                                                }).ToList();
+
+            List<SelectListItem> urunler = (from i in db.tbl_urun.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = i.Urunad,
+                                                Value = i.UrunID.ToString()
+                                            }).ToList();
+
+
+            ViewBag.mus = musteriler;
+            ViewBag.urn = urunler;
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult satisekle(tbl_satis s)
+        {
+            var prod = db.tbl_urun.Find(s.tbl_urun.UrunID);
+            if (s.Satisadet <= prod.UrunStok)
+            {
+                if(s.Satisadet==null || s.SatisFiyat==null ||  s.Satistarih==null)
+                {
+                    List<SelectListItem> musteriler = (from i in db.tbl_musteri.ToList()
+                                                       select new SelectListItem
+                                                       {
+                                                           Text = i.MusteriAdsoyad,
+                                                           Value = i.MusteriID.ToString()
+                                                       }).ToList();
+
+                    List<SelectListItem> urunler = (from i in db.tbl_urun.ToList()
+                                                    select new SelectListItem
+                                                    {
+                                                        Text = i.Urunad,
+                                                        Value = i.UrunID.ToString()
+                                                    }).ToList();
+
+
+                    ViewBag.mus = musteriler;
+                    ViewBag.urn = urunler;
+                    return View();
+                }
+                else
+                {
+                 var must = db.tbl_musteri.Where(x => x.MusteriID == s.tbl_musteri.MusteriID).FirstOrDefault();
+                 s.tbl_musteri = must;
+
+                 var urn = db.tbl_urun.Where(x => x.UrunID == s.tbl_urun.UrunID).FirstOrDefault();
+                    s.tbl_urun = urn;
+                    db.tbl_satis.Add(s);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+
+                }
+
+                
+            }
+            return View(s);
+        }
     }
 }
--- MVCSatisTakip/Controllers/UrunController.cs
+++ MVCSatisTakip/Controllers/UrunController.cs
@@ -13,9 +13,15 @@ namespace MVCSatisTakip.Controllers
     {
         satistakipEntities db = new satistakipEntities();
         // GET: Urun
-        public ActionResult Index(int sayfa = 1)
+        public ActionResult Index(int sayfa=1)
         {
             var urunler = db.tbl_urun.ToList().ToPagedList(sayfa, 7);
+            //var urun = from d in db.tbl_urun select d;
+            //if (!string.IsNullOrEmpty(p))
+            //{
+            //    urun = urun.Where(m => m.Urunad.Contains(p));
+            //}
+            //var urunler = db.tbl_urun.ToList();
             return View(urunler);
         }
 
@@ -24,11 +30,11 @@ namespace MVCSatisTakip.Controllers
             List<SelectListItem> kategoriler = (from i in db.tbl_kategori.ToList()
                                                 select new SelectListItem
                                                 {
-                            Text = i.KategoriAd,
-                            Value = i.KategoriID.ToString()
+                                                    Text = i.KategoriAd,
+                                                    Value = i.KategoriID.ToString()
                                                 }).ToList();
 
-            ViewBag.ktgr=kategoriler;
+            ViewBag.ktgr = kategoriler;
             return View();
 
         }
@@ -37,32 +43,33 @@ namespace MVCSatisTakip.Controllers
 
         public ActionResult Urunekle(tbl_urun p1)
         {
-            if (p1.Urunad==null ||p1.UrunFiyat==null || p1.UrunStok==null ||p1.Marka==null) { 
-            List<SelectListItem> kategoriler = (from i in db.tbl_kategori.ToList()
-                                                select new SelectListItem
-                                                {
-                                       Text = i.KategoriAd,
-                                Value = i.KategoriID.ToString()
-                                                }).ToList();
+            if (p1.Urunad == null || p1.UrunFiyat == null || p1.UrunStok == null || p1.Marka == null)
+            {
+                List<SelectListItem> kategoriler = (from i in db.tbl_kategori.ToList()
+                                                    select new SelectListItem
+                                                    {
+                                                        Text = i.KategoriAd,
+                                                        Value = i.KategoriID.ToString()
+                                                    }).ToList();
 
-            ViewBag.ktgr = kategoriler;
-            return View();
+                ViewBag.ktgr = kategoriler;
+                return View();
             }
             else
             {
-                var ktg=db.tbl_kategori.Where(m=>m.KategoriID==p1.tbl_kategori.KategoriID).FirstOrDefault();
-                p1.tbl_kategori=ktg;
+                var ktg = db.tbl_kategori.Where(m => m.KategoriID == p1.tbl_kategori.KategoriID).FirstOrDefault();
+                p1.tbl_kategori = ktg;
                 db.tbl_urun.Add(p1);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-        
+
 
         }
         public ActionResult Sil(int id)
         {
-            var urun =db.tbl_urun.Find(id);
+            var urun = db.tbl_urun.Find(id);
             db.tbl_urun.Remove(urun);
             db.SaveChanges();
             return RedirectToAction("Index");
@@ -73,14 +80,14 @@ namespace MVCSatisTakip.Controllers
             var urun = db.tbl_urun.Find(id);
 
             List<SelectListItem> urungetir = (from i in db.tbl_kategori.ToList()
-                                                select new SelectListItem
-                                                {
-                          Text = i.KategoriAd,
-                          Value = i.KategoriID.ToString()
-                                                }).ToList();
+                                              select new SelectListItem
+                                              {
+                                                  Text = i.KategoriAd,
+                                                  Value = i.KategoriID.ToString()
+                                              }).ToList();
 
             ViewBag.urun = urungetir;
-            return View("urungetir",urun);
+            return View("urungetir", urun);
 
         }
 
@@ -92,7 +99,7 @@ namespace MVCSatisTakip.Controllers
             urn.UrunStok = p.UrunStok;
             urn.UrunFiyat = p.UrunFiyat;
             var ktg = db.tbl_kategori.Where(m => m.KategoriID == p.tbl_kategori.KategoriID).FirstOrDefault();
-            urn.Kategori=ktg.KategoriID;
+            urn.Kategori = ktg.KategoriID;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
